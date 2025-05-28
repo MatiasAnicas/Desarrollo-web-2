640e4dff451dedae0fdf978a94e43a2005721596
MatiasAnicas <matiasanicas@fecea.edu.ar>
2025-05-28 11:28:08 -0300

    Desafio 7

---
+++ 7/app.js
@@ -0,0 +1,5 @@
+let precioProducto= document.getElementById("precioProducto")
+precioProducto.innerText="$450"
+
+let nombreProducto=document.getElementById("nombreProducto")
+nombreProducto.innerHTML=`<h1 style="color:red">Pan</h1>`
